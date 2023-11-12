eb50601fcd08417e2669f24b921ed87dde2cf49f
actuallyatoaster <7093583+actuallyatoaster@users.noreply.github.com>
2023-11-11 21:26:57 -0500

    Correctly batch acks

--- batch_acks.py
+++ batch_acks.py
@@ -2,30 +2,44 @@ import time
 import multiprocessing as mp
 from scapy.all import IP, TCP, send
 
+# TODO:
+# Measure RTT?
+# Sequence check
+# Try to make the vector
 
-def send_ack_for_pkt(pkt):
+# DONE:
+# Batch acks
+
+def make_ack_for_pkt(pkt):
     pkt_payload_len = len(bytes(pkt[TCP].payload))
+    if pkt_payload_len == 0: return
     ack_pkt = IP(dst=pkt[IP].src) / TCP(dport=80, sport=pkt[TCP].dport,
                  seq=pkt[TCP].ack, ack=pkt[TCP].seq + pkt_payload_len + 1, flags='A')
-    send(ack_pkt)
+    return ack_pkt
 
 def q_listen(pkt_q, rtt):
     while True:
         time.sleep(rtt)
         print("======== Begin RTT===========")
         acks_to_send = pkt_q.qsize()
-
+        acks = []
         for pkt_num in range(acks_to_send):
             pkt = pkt_q.get_nowait()
-            print(pkt.summary())
-            print(bytes(pkt[TCP].payload))
+            # print(pkt.summary())
+            # print(bytes(pkt[TCP].payload))
             
             if "F" in pkt[TCP].flags:
+                pkt_payload_len = len(bytes(pkt[TCP].payload))
+                fin_ack_pkt = IP(dst=pkt[IP].src) / TCP(dport=80, sport=pkt[TCP].dport,
+                                 seq=pkt[TCP].ack, ack=pkt[TCP].seq + pkt_payload_len + 1, flags='FA')
+                acks.append(fin_ack_pkt)
+                print("Sent finack")
                 pkt_q.close()
+                send(acks)
                 return
             #print(bytes(pkt[TCP].payload))
-            send_ack_for_pkt(pkt)
-
+            acks.append(make_ack_for_pkt(pkt))
+        send(acks)
 
 
 
--- scratch.py
+++ scratch.py
@@ -118,14 +118,14 @@ if __name__ == "__main__":
 	# 	curr_dir += '/'
 
 
-	RTT = 0.6
+	RTT = 0.8
 	MSS = 100
 	LOSS_CW = 128
 
 	# url='http.badssl.com'
-	url='www.thomas-bayer.com'
+	url='20.106.168.254'
 	dst_ip = socket.gethostbyname(url)
-	site_obj = f"http://{url}/"
+	site_obj = f"http://{url}/testfiles/100MBfile.txt"
 
 	site_obj = site_obj[7:]
 	if site_obj.find("/") != -1:
@@ -143,11 +143,12 @@ if __name__ == "__main__":
 	seq = random.randrange(100000,99999999)
 	src_port = random.randrange(20000,50000)
 
-	syn = IP(dst=dst_ip) / TCP(dport=80, sport=src_port, seq=seq, flags='S')
+	syn = IP(dst=dst_ip) / TCP(dport=80, sport=src_port, seq=seq, flags='S',
+							   options=[("MSS",MSS), ("NOP",None),('WScale', 10)])
 	syn_ack = sr1(syn)
 	print(syn_ack)
 	print(syn_ack[IP].src)
-	getStr = f'GET / HTTP/1.1\r\nHost: {url}\r\n\r\n'
+	getStr = f'GET {req_obj} HTTP/1.1\r\nHost: {url}\r\nConnection: Close\r\n\r\n'
 	request = IP(dst=f'{url}') / TCP(dport=80, sport=syn_ack[TCP].dport,
 				seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
 	send(request)
